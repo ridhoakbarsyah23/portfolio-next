f6f913e2c08fe538e866a797f91b7c40ac3d6160
@ridhoakbarsyah_ <dev.ridho.akbarsyah@gmail.com>
2025-11-13 19:15:23 +0700

    commit pertama

---
+++ src/app/layout.tsx
@@ -0,0 +1,19 @@
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./globals.css";
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "Ridho Akbarsyah - Portfolio",
+  description: "Website portfolio pribadi dengan Next.js + Bootstrap",
+};
+
+export default function RootLayout({ children }: { children: React.ReactNode }) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>{children}</body>
+    </html>
+  );
+}
